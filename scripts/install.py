56e9a81657947898e5904a3108c6453a712b1efe
Ethan Gilchrist <ethan@racecode.com>
2020-11-25 21:57:09 -0700

    Fix: pip3 notes

--- scripts/install.py
+++ scripts/install.py
@@ -4,8 +4,8 @@ import subprocess
 print('Downloading Necessary Packages...')
 listOfPackages = ['PySimpleGUI', 'matplotlib', 'faker', 'psycopg2', 'psycopg2-binary']
 for i in listOfPackages:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', i])
-    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    subprocess.check_call([sys.executable, '-m', 'pip3', 'install', i])
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip3', 'freeze'])
     installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
     print('Completed Downloading', i)
 
